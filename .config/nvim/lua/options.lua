f3548276fc08b243606c2342a4fcc162fb00411e
codeadpool <72756153+codeadpool@users.noreply.github.com>
2025-01-25 12:55:15 -0500

    Initial commit

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.clipboard:append("unnamed")
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.relativenumber = true
+vim.opt.number = true
